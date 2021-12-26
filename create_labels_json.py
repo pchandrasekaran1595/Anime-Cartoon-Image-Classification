db15c2e8ac435809937062ce2f306203c6bf9bc2
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-12-27 00:17:02 +0530

    Create Labels in JSON Format

--- create_labels_json.py
+++ create_labels_json.py
@@ -5,17 +5,8 @@ import json
 def main():
 
     labels = {
-        "0"  : "Dew",
-        "1"  : "Fog Smog",
-        "2"  : "Frost",
-        "3"  : "Glaze",
-        "4"  : "Hail",
-        "5"  : "Lightning",
-        "6"  : "Rain",
-        "7"  : "Rainbow",
-        "8"  : "Rime",
-        "9"  : "Sandstorm",
-        "10" : "Snow"
+        "0"  : "Anime",
+        "1"  : "Cartoon",
     }
 
     json.dump(labels, open("labels.json", "w"))
